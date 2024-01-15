64000f5bf710b4f7cfcd7a6947abcd5e8965d61b
Krushna Waghumbare <waghumbarekrushna143@gmail.com>
2024-01-15 22:54:40 +0530

    Design all and add total count and clear all button

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -20,21 +20,26 @@ const Todo =()=>{
       setTodos(newtodos);
     }
 
+    const clearAll=()=>{
+
+      const cleartodos=[];
+      setTodos(cleartodos);
+    }
     return(
       
        <div className="container d-flex justify-content-center align-items-center">
-          <div className="col-12 col-md-6 bg-primary my-5">
+          <div className="col-12 col-md-6 bgcolor my-5">
 
-          <div className=" my-4 text-center">
-             <input type="text" style={{width:'70%'}}
+          <div className=" my-4 text-center input11">
+             <input type="text" style={{width:'80%',height:'35px' ,textAlign:'center'}}
                name='userName'
                value={todo}   
                onChange={(e) => setTodo(e.target.value)}
-               placeholder='Enter your name'
+               placeholder='Enter your tasks'
              />
 
 
-            <button className='mx-2' onClick={addTodo}>Add Todo</button>
+            <button className='mx-2 btn btn-primary' onClick={addTodo}><i class="fa fa-plus" aria-hidden="true"></i></button>
           </div>  
         
           <div className='p-4'>
@@ -42,8 +47,8 @@ const Todo =()=>{
           <table className='table table-bordered table-responsive '>
               <tbody>
               <tr className='text-center'>
-                <th>Name</th>
-                <th>Delete</th>
+                <th style={{width:"80%"}}>Tasks</th>
+                <th style={{width:"20%"}}>Delete</th>
               </tr>
               </tbody>
              {            
@@ -51,14 +56,25 @@ const Todo =()=>{
                 return(
                  <tbody>
                     <tr key={index} className='text-center'>
-                    <td>{val}</td>
-                    <td><button className='btn btn-danger ' onClick={()=>removetodos(index)}>Delete</button></td>
+                    <td className='tasks'>{val}</td>
+                    <td><button className='btn btn-danger ' onClick={()=>removetodos(index)}><i class="fas fa-trash-alt"></i></button></td>
                    </tr>
                  </tbody>
                 );
               })}
              </table>
 
+          </div>
+
+          <div>
+             <div className="row  mb-3">
+               <div className="col-md-8 col-12 text-center text-white my-1">
+                    <h5>You have {todos.length} pending task ...</h5>
+               </div>
+               <div className="col-md-4 col-12 text-center">
+                      <button className='btn btn-dark' onClick={clearAll}>Clear all</button>
+               </div>
+             </div>
           </div>
 
           </div>
